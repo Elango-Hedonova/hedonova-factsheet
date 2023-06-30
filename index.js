40926f921bb98b11cf3bd7fce090c35a1051b3be
Elangodevguy <elangodevguy@gmail.com>
2023-06-30 12:48:32 +0530

    added two more section

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const sheets = google.sheets("v4");
 const app = express();
 require("dotenv").config();
 
+app.get("/", async (req, res) => {
+  try {
+    res.status(200).json({ text: "fact sheet" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal server error");
+  }
+});
 // Define the API route to fetch data from the Google Sheet
 app.get("/api/factsheet/header-section", async (req, res) => {
   try {
@@ -15,7 +23,99 @@ app.get("/api/factsheet/header-section", async (req, res) => {
 
     const client = await auth.getClient();
     const spreadsheetId = "1__uoCp5ZIQKpY2YmhPdpqr5CjOXo-8-3I1nP4MSc_YQ"; // Replace with your own spreadsheet ID
-    const range = "Sheet2"; // Replace with your own sheet name
+    const range = "Header section"; // Replace with your own sheet name
+    const response = await sheets.spreadsheets.values.get({
+      auth: client,
+      spreadsheetId,
+      range,
+    });
+
+    const rows = response.data.values;
+    const headers = rows[0];
+    const values = rows[1];
+    const outputObj = {};
+
+    for (let i = 0; i < headers.length; i++) {
+      const header = headers[i];
+      const value = values[i];
+      const properties = header.split(".");
+
+      let currentObj = outputObj;
+      for (let j = 0; j < properties.length - 1; j++) {
+        const property = properties[j];
+        if (!currentObj[property]) {
+          currentObj[property] = {};
+        }
+        currentObj = currentObj[property];
+      }
+
+      const lastProperty = properties[properties.length - 1];
+      currentObj[lastProperty] = value;
+    }
+
+    res.json(outputObj);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal server error");
+  }
+});
+
+app.get("/api/factsheet/fund-facts", async (req, res) => {
+  try {
+    const auth = new google.auth.GoogleAuth({
+      credentials: keys,
+      scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
+    });
+
+    const client = await auth.getClient();
+    const spreadsheetId = "1__uoCp5ZIQKpY2YmhPdpqr5CjOXo-8-3I1nP4MSc_YQ"; // Replace with your own spreadsheet ID
+    const range = "Fund facts"; // Replace with your own sheet name
+    const response = await sheets.spreadsheets.values.get({
+      auth: client,
+      spreadsheetId,
+      range,
+    });
+
+    const rows = response.data.values;
+    const headers = rows[0];
+    const values = rows[1];
+    const outputObj = {};
+
+    for (let i = 0; i < headers.length; i++) {
+      const header = headers[i];
+      const value = values[i];
+      const properties = header.split(".");
+
+      let currentObj = outputObj;
+      for (let j = 0; j < properties.length - 1; j++) {
+        const property = properties[j];
+        if (!currentObj[property]) {
+          currentObj[property] = {};
+        }
+        currentObj = currentObj[property];
+      }
+
+      const lastProperty = properties[properties.length - 1];
+      currentObj[lastProperty] = value;
+    }
+
+    res.json(outputObj);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal server error");
+  }
+});
+
+app.get("/api/factsheet/fees-minimums", async (req, res) => {
+  try {
+    const auth = new google.auth.GoogleAuth({
+      credentials: keys,
+      scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
+    });
+
+    const client = await auth.getClient();
+    const spreadsheetId = "1__uoCp5ZIQKpY2YmhPdpqr5CjOXo-8-3I1nP4MSc_YQ"; // Replace with your own spreadsheet ID
+    const range = "Fees & minimums"; // Replace with your own sheet name
     const response = await sheets.spreadsheets.values.get({
       auth: client,
       spreadsheetId,
