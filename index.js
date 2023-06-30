9730226eff9e4e660eda041c6117850afca35f24
Elangodevguy <elangodevguy@gmail.com>
2023-06-30 13:44:24 +0530

    added weight exposure section

--- index.js
+++ index.js
@@ -152,6 +152,75 @@ app.get("/api/factsheet/fees-minimums", async (req, res) => {
   }
 });
 
+app.get("/api/factsheet/weighted-exposure", async (req, res) => {
+  try {
+    const auth = new google.auth.GoogleAuth({
+      credentials: keys,
+      scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
+    });
+
+    const client = await auth.getClient();
+    const spreadsheetId = "1__uoCp5ZIQKpY2YmhPdpqr5CjOXo-8-3I1nP4MSc_YQ"; // Replace with your own spreadsheet ID
+    const range = "Weighted exposure"; // Replace with your own sheet name
+    const response = await sheets.spreadsheets.values.get({
+      auth: client,
+      spreadsheetId,
+      range,
+    });
+    const spreadsheetId2 = "1__uoCp5ZIQKpY2YmhPdpqr5CjOXo-8-3I1nP4MSc_YQ"; // Replace with your own spreadsheet ID
+    const range2 = "Weighted exposure section"; // Replace with your own sheet name
+    const response2 = await sheets.spreadsheets.values.get({
+      auth: client,
+      spreadsheetId: spreadsheetId2,
+      range: range2,
+    });
+    const rows2 = response2.data.values;
+    const headers2 = rows2[0];
+    const values2 = rows2[1];
+    const outputObj = {};
+
+    for (let i = 0; i < headers2.length; i++) {
+      const header = headers2[i];
+      const value = values2[i];
+      const properties = header.split(".");
+
+      let currentObj = outputObj;
+      for (let j = 0; j < properties.length - 1; j++) {
+        const property = properties[j];
+        if (!currentObj[property]) {
+          currentObj[property] = {};
+        }
+        currentObj = currentObj[property];
+      }
+
+      const lastProperty = properties[properties.length - 1];
+      currentObj[lastProperty] = value;
+    }
+
+    // res.json(outputObj);
+    const rows = response.data.values;
+    const header = rows[0];
+    const values = rows.slice(1);
+    const result = values.map((row) => {
+      const obj = {};
+      header.forEach((key, i) => {
+        obj[key] = row[i];
+      });
+      return obj;
+    });
+
+    const final = {
+      section: outputObj.section,
+      data: result,
+    };
+
+    res.json(final);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal server error");
+  }
+});
+
 // Start the server
 const PORT = process.env.PORT || 3030;
 app.listen(PORT, () => {
