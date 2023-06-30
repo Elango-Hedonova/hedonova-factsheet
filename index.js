bdb95a002c1c30e1819b2a5896e9a2fa47dc7ddb
Elangodevguy <elangodevguy@gmail.com>
2023-06-30 14:13:06 +0530

    added closing price section

--- index.js
+++ index.js
@@ -220,6 +220,51 @@ app.get("/api/factsheet/weighted-exposure", async (req, res) => {
     res.status(500).send("Internal server error");
   }
 });
+app.get("/api/factsheet/closing-price", async (req, res) => {
+  try {
+    const auth = new google.auth.GoogleAuth({
+      credentials: keys,
+      scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
+    });
+
+    const client = await auth.getClient();
+    const spreadsheetId = "1__uoCp5ZIQKpY2YmhPdpqr5CjOXo-8-3I1nP4MSc_YQ"; // Replace with your own spreadsheet ID
+    const range = "Closing price"; // Replace with your own sheet name
+    const response = await sheets.spreadsheets.values.get({
+      auth: client,
+      spreadsheetId,
+      range,
+    });
+
+    const rows = response.data.values;
+    const headers = rows[0];
+    const values = rows[1];
+    const outputObj = {};
+
+    for (let i = 0; i < headers.length; i++) {
+      const header = headers[i];
+      const value = values[i];
+      const properties = header.split(".");
+
+      let currentObj = outputObj;
+      for (let j = 0; j < properties.length - 1; j++) {
+        const property = properties[j];
+        if (!currentObj[property]) {
+          currentObj[property] = {};
+        }
+        currentObj = currentObj[property];
+      }
+
+      const lastProperty = properties[properties.length - 1];
+      currentObj[lastProperty] = value;
+    }
+
+    res.json(outputObj);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal server error");
+  }
+});
 
 // Start the server
 const PORT = process.env.PORT || 3030;
