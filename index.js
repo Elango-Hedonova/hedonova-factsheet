8bd65b7e7e4bc77b6cb10512a628e4675076ee7b
Elangodevguy <elangodevguy@gmail.com>
2023-07-07 15:36:06 +0530

    added alpha chart

--- index.js
+++ index.js
@@ -387,6 +387,46 @@ app.get("/api/factsheet/standard-deviation-chart", async (req, res) => {
     res.status(500).send("Internal server error");
   }
 });
+app.get("/api/factsheet/alpha-chart", async (req, res) => {
+  try {
+    const auth = new google.auth.GoogleAuth({
+      credentials: keys,
+      scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
+    });
+
+    const client = await auth.getClient();
+    const spreadsheetId = "19GRNwJ8_u3UBbIGrxsTtij27FXt6N-JGh1RFlmSRWic"; // Replace with your own spreadsheet ID
+    const range = "Alpha updated"; // Replace with your own sheet name
+    const response = await sheets.spreadsheets.values.get({
+      auth: client,
+      spreadsheetId,
+      range,
+    });
+
+    const rows = response.data.values;
+    const header = rows[0];
+    const values = rows.slice(1);
+    const result = values.map((row) => {
+      const obj = {};
+      header.forEach((key, i) => {
+        obj[key] = row[i];
+      });
+      return obj;
+    });
+
+    res.json(
+      result
+        .filter((el) => !isNaN(el["Alpha"]))
+        .map((el) => ({
+          date: el["date"],
+          alpha: Number(el["Alpha"]),
+        }))
+    );
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal server error");
+  }
+});
 // Start the server
 const PORT = process.env.PORT || 3030;
 app.listen(PORT, () => {
