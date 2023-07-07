99ebec1a551be5627bba575a0366822066e40e05
Elangodevguy <elangodevguy@gmail.com>
2023-07-07 14:26:15 +0530

    added standard deviation chart

--- index.js
+++ index.js
@@ -346,6 +346,47 @@ app.get("/api/factsheet/beta-chart", async (req, res) => {
     res.status(500).send("Internal server error");
   }
 });
+app.get("/api/factsheet/standard-deviation-chart", async (req, res) => {
+  try {
+    const auth = new google.auth.GoogleAuth({
+      credentials: keys,
+      scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
+    });
+
+    const client = await auth.getClient();
+    const spreadsheetId = "19GRNwJ8_u3UBbIGrxsTtij27FXt6N-JGh1RFlmSRWic"; // Replace with your own spreadsheet ID
+    const range = "Standard deviation"; // Replace with your own sheet name
+    const response = await sheets.spreadsheets.values.get({
+      auth: client,
+      spreadsheetId,
+      range,
+    });
+
+    const rows = response.data.values;
+    const header = rows[0];
+    const values = rows.slice(1);
+    const result = values.map((row) => {
+      const obj = {};
+      header.forEach((key, i) => {
+        obj[key] = row[i];
+      });
+      return obj;
+    });
+
+    res.json(
+      result
+        .filter((el) => el["Hedonova Std Deviation (60 day moving average)"])
+        .map((el) => ({
+          date: el["date"],
+          hedonova: el["Hedonova Std Deviation (60 day moving average)"],
+          "s&p500": el["S&P 500 Std Deviation (60 day moving average)"],
+        }))
+    );
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal server error");
+  }
+});
 // Start the server
 const PORT = process.env.PORT || 3030;
 app.listen(PORT, () => {
