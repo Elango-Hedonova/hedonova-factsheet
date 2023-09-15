b9872cabcf76dc8cafe07cb3b03a57df0fed29b1
Elangodevguy <elangodevguy@gmail.com>
2023-09-15 10:01:25 +0530

    added annualised return

--- index.js
+++ index.js
@@ -616,7 +616,51 @@ app.get("/api/charts/rolling-correlation", async (req, res) => {
     res.status(500).send("Internal server error");
   }
 });
+app.get("/api/portfolio/annualized-return", async (req, res) => {
+  try {
+    const auth = new google.auth.GoogleAuth({
+      credentials: keys,
+      scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
+    });
 
+    const client = await auth.getClient();
+    const spreadsheetId = "19GRNwJ8_u3UBbIGrxsTtij27FXt6N-JGh1RFlmSRWic"; // Replace with your own spreadsheet ID
+    const range = "Annualized return"; // Replace with your own sheet name
+    const response = await sheets.spreadsheets.values.get({
+      auth: client,
+      spreadsheetId,
+      range,
+    });
+
+    const rows = response.data.values;
+    const headers = rows[0];
+    const values = rows[1];
+    const outputObj = {};
+
+    for (let i = 0; i < headers.length; i++) {
+      const header = headers[i];
+      const value = values[i];
+      const properties = header.split(".");
+
+      let currentObj = outputObj;
+      for (let j = 0; j < properties.length - 1; j++) {
+        const property = properties[j];
+        if (!currentObj[property]) {
+          currentObj[property] = {};
+        }
+        currentObj = currentObj[property];
+      }
+
+      const lastProperty = properties[properties.length - 1];
+      currentObj[lastProperty] = value;
+    }
+
+    res.json(outputObj);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal server error");
+  }
+});
 // Start the server
 const PORT = process.env.PORT || 3030;
 app.listen(PORT, () => {
