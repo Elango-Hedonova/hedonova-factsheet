2faf20bc25f2d94eec1c365d84409f27c75223e3
Elangodevguy <elangodevguy@gmail.com>
2023-10-20 13:25:57 +0530

    added continent and name in map

--- map.js
+++ map.js
@@ -1,5 +1,7 @@
 const map = {
   US: {
+    continent: "north_america",
+    name: "United States",
     holdings: [
       { name: "Real estate", percentage: "10" },
       { name: "Equipment finance", percentage: "6" },
@@ -7,6 +9,8 @@ const map = {
     ],
   },
   IN: {
+    continent: "asia",
+    name: "India",
     holdings: [
       { name: "Real estate", percentage: "10" },
       { name: "Equipment finance", percentage: "6" },
