62e0f906b74bc96f22a6308db0d69244550667f4
Elangodevguy <elangodevguy@gmail.com>
2023-12-07 19:28:22 +0530

    added real world data for the map

--- map.js
+++ map.js
@@ -3,22 +3,111 @@ const map = {
     continent: "north_america",
     name: "United States",
     holdings: [
-      { name: "Real estate", percentage: "10" },
-      { name: "Equipment finance", percentage: "6" },
-      { name: "Startups", percentage: "11" },
+      { name: "Real estate", percentage: "1" },
+      { name: "Litigation finance", percentage: "1" },
+      { name: "Startups", percentage: "6" },
+      { name: "Agronomy", percentage: "2" },
+      { name: "Music royalties", percentage: "2" },
+      { name: "Art", percentage: "1" },
     ],
   },
   IN: {
     continent: "asia",
     name: "India",
     holdings: [
-      { name: "Real estate", percentage: "10" },
-      { name: "Equipment finance", percentage: "6" },
-      { name: "Startups", percentage: "11" },
+      { name: "Real estate", percentage: "2" },
+      { name: "Litigation finance", percentage: "2" },
+      { name: "Startups", percentage: "3" },
+      { name: "Equipment financing", percentage: "2" },
+      { name: "Music royalties", percentage: "1" },
+      { name: "Art", percentage: "3" },
     ],
+  },
+  FR: {
+    continent: "europe",
+    name: "France",
+    holdings: [
+      { name: "Startups", percentage: "1" },
+      { name: "Spirits", percentage: "2" },
+      { name: "Art", percentage: "2" },
+    ],
+  },
+  GB: {
+    continent: "europe",
+    name: "United Kingdom",
+    holdings: [
+      { name: "Litigation finance", percentage: "1" },
+      { name: "Equipment financing", percentage: "1" },
+      { name: "Art", percentage: "2" },
+    ],
+  },
+  ES: {
+    continent: "europe",
+    name: "Spain",
+    holdings: [{ name: "Equipment financing", percentage: "1" }],
+  },
+  IT: {
+    continent: "europe",
+    name: "Italy",
+    holdings: [{ name: "Spirits", percentage: "1" }],
+  },
+  DE: {
+    continent: "europe",
+    name: "Germany",
+    holdings: [
+      { name: "Spirits", percentage: "1" },
+      { name: "Startups", percentage: "1" },
+    ],
+  },
+  NO: {
+    continent: "europe",
+    name: "Norway",
+    holdings: [{ name: "Music royalties", percentage: "1" }],
+  },
+  AU: {
+    continent: "australia",
+    name: "Australia",
+    holdings: [{ name: "Litigation finance", percentage: "1" }],
+  },
+  NP: {
+    continent: "asia",
+    name: "Nepal",
+    holdings: [{ name: "Real estate", percentage: "1" }],
+  },
+  TH: {
+    continent: "asia",
+    name: "Thailand",
+    holdings: [{ name: "Equipment financing", percentage: "1" }],
+  },
+  VN: {
+    continent: "asia",
+    name: "Vietnam",
+    holdings: [{ name: "Real estate", percentage: "1" }],
+  },
+  CN: {
+    continent: "asia",
+    name: "China",
+    holdings: [
+      { name: "Equipment financing", percentage: "1" },
+      { name: "Agronomy", percentage: "1" },
+      { name: "Art", percentage: "1" },
+    ],
+  },
+  GH: {
+    continent: "africa",
+    name: "Ghana",
+    holdings: [{ name: "Agronomy", percentage: "1" }],
   },
 };
 
 module.exports = {
   map,
 };
+
+// africa
+// asia
+// europe
+// north_america
+// south_america
+// antarctica
+// australia
