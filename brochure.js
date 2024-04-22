4845260a4b609da7b97a019b013f61468e65089a
Elangodevguy <elangodevguy@gmail.com>
2024-04-22 14:17:47 +0530

    changed brochure to 90 days

--- brochure.js
+++ brochure.js
@@ -12,6 +12,45 @@ function isLastDayOfMonth(date) {
   return nextDay.getMonth() !== date.getMonth();
 }
 
+// async function sharpe_chart() {
+//   try {
+//     const auth = new google.auth.GoogleAuth({
+//       credentials: keys,
+//       scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
+//     });
+
+//     const client = await auth.getClient();
+//     const spreadsheetId = "19GRNwJ8_u3UBbIGrxsTtij27FXt6N-JGh1RFlmSRWic"; // Replace with your own spreadsheet ID
+//     const range = "Sharpe updated monthly"; // Replace with your own sheet name
+//     const response = await sheets.spreadsheets.values.get({
+//       auth: client,
+//       spreadsheetId,
+//       range,
+//     });
+
+//     const rows = response.data.values;
+//     const header = rows[0];
+//     const values = rows.slice(1);
+//     const result = values.map((row) => {
+//       const obj = {};
+//       header.forEach((key, i) => {
+//         obj[key] = row[i];
+//       });
+//       return obj;
+//     });
+
+//     return result
+//       .filter((el) => el["60 day Rolling Sharpe ratio"])
+//       .filter((el) => isLastDayOfMonth(new Date(el["Date"])))
+//       .map((el) => ({
+//         date: el["Date"],
+//         sharpe_ratio: Number(el["60 day Rolling Sharpe ratio"]).toFixed(2) * 1,
+//       }));
+//   } catch (error) {
+//     return error;
+//   }
+// }
+
 async function sharpe_chart() {
   try {
     const auth = new google.auth.GoogleAuth({
@@ -21,7 +60,7 @@ async function sharpe_chart() {
 
     const client = await auth.getClient();
     const spreadsheetId = "19GRNwJ8_u3UBbIGrxsTtij27FXt6N-JGh1RFlmSRWic"; // Replace with your own spreadsheet ID
-    const range = "Sharpe updated monthly"; // Replace with your own sheet name
+    const range = "Rolling sharpe latest"; // Replace with your own sheet name
     const response = await sheets.spreadsheets.values.get({
       auth: client,
       spreadsheetId,
@@ -40,17 +79,55 @@ async function sharpe_chart() {
     });
 
     return result
-      .filter((el) => el["60 day Rolling Sharpe ratio"])
-      .filter((el) => isLastDayOfMonth(new Date(el["Date"])))
+      .filter((el) => el["90 day Rolling Sharpe ratio"])
       .map((el) => ({
         date: el["Date"],
-        sharpe_ratio: Number(el["60 day Rolling Sharpe ratio"]).toFixed(2) * 1,
+        sharpe_ratio: Number(el["90 day Rolling Sharpe ratio"]).toFixed(2) * 1,
       }));
   } catch (error) {
+    console.error(error);
     return error;
   }
 }
 
+// async function rolling_correlation() {
+//   try {
+//     const auth = new google.auth.GoogleAuth({
+//       credentials: keys,
+//       scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
+//     });
+
+//     const client = await auth.getClient();
+//     const spreadsheetId = "19GRNwJ8_u3UBbIGrxsTtij27FXt6N-JGh1RFlmSRWic"; // Replace with your own spreadsheet ID
+//     const range = "Rolling correlation - month end email"; // Replace with your own sheet name
+//     const response = await sheets.spreadsheets.values.get({
+//       auth: client,
+//       spreadsheetId,
+//       range,
+//     });
+
+//     const rows = response.data.values;
+//     const header = rows[0];
+//     const values = rows.slice(1);
+//     const result = values.map((row) => {
+//       const obj = {};
+//       header.forEach((key, i) => {
+//         obj[key] = row[i];
+//       });
+//       return obj;
+//     });
+
+//     return result
+//       .filter((el) => el["60 day rolling correlation"])
+//       .filter((el) => isLastDayOfMonth(new Date(el["date"])))
+//       .map((el) => ({
+//         date: el["date"],
+//         rolling_correlation: Number(el["60 day rolling correlation"]),
+//       }));
+//   } catch (error) {
+//     return error;
+//   }
+// }
 async function rolling_correlation() {
   try {
     const auth = new google.auth.GoogleAuth({
@@ -79,17 +156,15 @@ async function rolling_correlation() {
     });
 
     return result
-      .filter((el) => el["60 day rolling correlation"])
-      .filter((el) => isLastDayOfMonth(new Date(el["date"])))
+      .filter((el) => el["90 day rolling correlation"])
       .map((el) => ({
         date: el["date"],
-        rolling_correlation: Number(el["60 day rolling correlation"]),
+        rolling_correlation: Number(el["90 day rolling correlation"]),
       }));
   } catch (error) {
     return error;
   }
 }
-
 async function hedVsSp500() {
   try {
     const auth = new google.auth.GoogleAuth({
